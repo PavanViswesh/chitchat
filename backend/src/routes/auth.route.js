008c2e200227370b85d30cf2c10b22d293bfe22c
PavanViswesh <pavanviswesh25@gmail.com>
2025-12-31 14:39:16 +0530

    error

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,6 @@ import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import { signup,login,logout,updateProfile } from "../controllers/auth.controller.js";
 import { arcjetProtection } from "../middleware/arcjet.middleware.js";
-import { updateProfile } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
